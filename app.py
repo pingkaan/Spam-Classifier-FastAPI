d67c07210f7ce6678404e2ffc7e859d8d65fcdc7
pingkaan <148237934+pingkaan@users.noreply.github.com>
2026-01-02 09:27:46 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+import os
+from fastapi import FastAPI, Request
+from fastapi.response import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+import pickle
+import numpy as np
+
+# path setup (PENTING)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# load model
+MODEL_PATH = os.path.join(BASE_DIR, "svm_model.pkl")
+
+with open(MODEL_PATH, "rb") as f:
+    model = pickle.load(f)
+
+# inisialisasi FastAPI
+app = FastAPI(
+    title = "Spam EMAIL Classification API",
+    description = "API klasifikasi email spam / ham menggunakan SVM",
+    version = "1.0.0"
+)
+
+# template
+templates = Jinja2Templates(
+    directory = os.path.join(BASE_DIR, "templates")
+)
+
+# routea
+@app.get("/", response_class = HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
+
+# schemas
+class EmailRequest(BaseModel):
+    email: str
+
+class EmailResponse(BaseModel):
+    prediction: str
+    confidence: float
+
+# prediksi endpoint
+@app.post("/predict", response_model = EmailResponse)
+async def predict_email(request: EmailRequest):
+    X = [request.email]
+
+    prediction = model.predict(X) [0]
+    probability = model.predict_proba(X).max()
+
+    return {
+        "prediction": str(prediction),
+        "confidence": round(float(probability), 4)
+    }
